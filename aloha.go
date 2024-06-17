eaf7c18dc984dc109d3b92ca49f1585a016e634f
Hyori 효리 <hyorigo@icloud.com>
2024-06-17 23:02:57 +0800

    First old

---
+++ aloha.go
@@ -0,0 +1,7 @@
+package previewread
+
+// PreviewRead doesn't read the first 100 bytes of a file.
+func PreviewRead() {
+	// Read the first 100 bytes of a file
+	// and print them to the console
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PureMature/previewread
+
+go 1.18
