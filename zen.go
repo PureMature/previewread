cda9991367123b1e97161722dac5a44316f18c92
Hyori 효리 <hyorigo@icloud.com>
2024-06-18 00:22:20 +0800

    names

--- zen.go
+++ zen.go
@@ -4,36 +4,32 @@
 //
 // Starbox supports loading modules from various sources, including built-in modules from Starlet, custom modules added by the user, and dynamic modules resolved by name on demand.
 //
-// Built-in Modules
+// Built-in Modules:
 //
-// Use SetModuleSet(modSet ModuleSetName) to select a predefined set of modules to preload before execution. Available sets include:
+// Use SetModuleSet(modSet ModuleSetName) to select a predefined set of modules to preload before execution.
+// Available sets include:
 //   - EmptyModuleSet: No modules.
 //   - SafeModuleSet: Safe modules without access to the file system or network.
 //   - NetworkModuleSet: Safe modules plus network modules.
 //   - FullModuleSet: All available modules.
 //
-// Adding Custom Modules
+// Custom Modules:
 //
-// - AddModuleLoader(moduleName string, moduleLoader starlet.ModuleLoader): Adds a custom module loader. Members can be accessed in the script via load("module_name", "member_name") or member_name.
-// - AddModuleFunctions(name string, funcs FuncMap): Adds a module with custom functions. Functions can be accessed in the script via load("module_name", "func_name") or module_name.func_name.
-// - AddModuleData(moduleName string, moduleData starlark.StringDict): Adds a module with custom data. Data can be accessed in the script via load("module_name", "key") or module_name.key.
-// - AddStructFunctions(name string, funcs FuncMap): Adds a module with custom struct functions. Functions can be accessed in the script via load("struct_name", "func_name") or struct_name.func_name.
-// - AddStructData(structName string, structData starlark.StringDict): Adds a module with custom struct data. Data can be accessed in the script via load("struct_name", "key") or struct_name.key.
+//   - AddModuleLoader(moduleName string, moduleLoader starlet.ModuleLoader): Adds a custom module loader. Members can be accessed in the script via load("module_name", "member_name") or member_name.
+//   - AddModuleFunctions(name string, funcs FuncMap): Adds a module with custom functions. Functions can be accessed in the script via load("module_name", "func_name") or module_name.func_name.
+//   - AddModuleData(moduleName string, moduleData starlark.StringDict): Adds a module with custom data. Data can be accessed in the script via load("module_name", "key") or module_name.key.
+//   - AddStructFunctions(name string, funcs FuncMap): Adds a module with custom struct functions. Functions can be accessed in the script via load("struct_name", "func_name") or struct_name.func_name.
+//   - AddStructData(structName string, structData starlark.StringDict): Adds a module with custom struct data. Data can be accessed in the script via load("struct_name", "key") or struct_name.key.
 //
-// Dynamic Module Loading
+// Dynamic Modules:
 //
-// - SetDynamicModuleLoader(loader DynamicModuleLoader): Sets a dynamic module loader, which loads modules based on their names before execution.
-//
-// Adding Named Modules
-//
-// - AddNamedModules(moduleNames ...string): Adds built-in or custom modules by their names.
-// - AddModulesByName(moduleNames ...string): Alias for AddNamedModules.
+//   - SetDynamicModuleLoader(loader DynamicModuleLoader): Sets a dynamic module loader, which loads modules based on their names before execution, i.e. module names added by AddNamedModules or AddModulesByName.
 //
 // # Module Loading Priority
 //
 // Modules are loaded in the following order of priority before execution:
-// 1. Preloaded Starlet modules from predefined sets and named Starlet modules.
-// 2. Custom modules added by users, overriding any preloaded Starlet modules with the same names.
-// 3. Dynamically loaded modules based on their names just before execution.
-// 4. If a module name is not found in any of the preloaded, custom, or dynamic modules, an error is returned.
+//   1. Preloaded Starlet modules from predefined sets and additional Starlet modules by name.
+//   2. Custom modules added by users, preloaded Starlet modules with the same names would not be overwritten.
+//   3. Dynamically loaded modules based on their names just before execution.
+//   4. If a module name is not found in any of the built-in, custom, or dynamic modules, an error is returned.
 package previewread
